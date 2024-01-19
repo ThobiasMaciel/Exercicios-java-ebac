86bbfed611b5c42b2213491d120860a43e6f7690
Thobias <thobias.f.maciel@gmail.com>
2024-01-19 16:16:29 -0300

    exercicio de media

---
+++ App.java
@@ -0,0 +1,15 @@
+package exercicioEbac2;
+
+public class App {
+   public static void main(String[] args) {
+    Media m1 = new Media();
+
+    m1.setNota1(5);
+    m1.setNota2(2);
+    m1.setNota3(7);
+    m1.setNota4(9);
+
+    m1.fazerMedia();
+   }
+    
+}
